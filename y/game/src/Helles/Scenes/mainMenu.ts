67ce0fb0b4a52e783c21456aed72ea5ab06fa9f2
523243240 <yunjia.ni@stonybrook.edu>
2024-03-30 12:22:15 -0400

    first commit

---
+++ y/game/src/Helles/Scenes/mainMenu.ts
@@ -0,0 +1,13 @@
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import Button from "../../Wolfie2D/Nodes/UIElements/Button";
+import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
+import Scene from "../../Wolfie2D/Scene/Scene";
+import Color from "../../Wolfie2D/Utils/Color";
+
+export default class MainMenu extends Scene{
+
+    
+
+}
---
+++ y/game/src/main.ts
@@ -0,0 +1,27 @@
+import Game from "./Wolfie2D/Loop/Game";
+
+(function main(){
+    //Run and tests 
+
+    runTests();
+
+    //Set up map size and input 
+    let options = {
+        canvasSize: {x:1200, y:800},
+        clearColor: {r: 34, g: 32, b:52},
+        inputs:[
+            {name: "left", keys: ["a"]},
+            {name: "right", keys: ["d"]},
+            {name: "jump", keys: ["w", "k"]},
+            {name: "attack", keys: [ "j"]},
+            {name: "skill", keys: ["q"]}
+        ],
+        useWebGL: false, 
+        showDebug: false  
+    }
+    const game = new Game(options);
+
+    // game.start(MainMenu,{});
+})();
+
+function runTests(){};
