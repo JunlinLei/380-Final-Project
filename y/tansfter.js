c13febd4fa16fa277b400368e72f6ef0c7c85d8c
JunlinLei <99279647+JunlinLei@users.noreply.github.com>
2024-04-08 19:40:45 -0400

    trial

---
+++ y/tansfter.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+function game() {
+	
+	return (
+		<p>Hi111111</p>
+	);
+};
+
+game();
